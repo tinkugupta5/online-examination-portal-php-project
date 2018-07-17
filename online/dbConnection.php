7861c5dda628b9812de2175b2247e11d71c820b3
tinkugupta5 <36808633+tinkugupta5@users.noreply.github.com>
2018-07-17 20:56:50 +0530

    Add files via upload

---
+++ online/dbConnection.php
@@ -0,0 +1,7 @@
+<?php
+//all the variables defined here are accessible in all the files that include this one
+//$con= new mysqli('localhost','root','','project')or die("Could not connect to mysql".mysqli_error($con));
+$con=mysqli_connect("localhost","root","","online examination");
+
+
+?>
